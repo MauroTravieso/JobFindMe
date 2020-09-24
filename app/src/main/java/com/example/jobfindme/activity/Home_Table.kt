de513b4c6b4db72356cb5bcd6cad1b3b2e446784
tariktaci <tariktaci>
2020-09-24 14:33:33 -0500

    Home fourth Commit

--- app/src/main/java/com/example/jobfindme/activity/Home_Table.kt
+++ app/src/main/java/com/example/jobfindme/activity/Home_Table.kt
@@ -8,38 +8,35 @@ import com.example.jobfindme.database.DBcontrollerJob
 import android.content.Context
 import kotlinx.android.synthetic.main.activity_home__table.*
 
-class Home_Table(c:Context) : AppCompatActivity() {
+class Home_Table() : AppCompatActivity() {
 
-    var ourContext: Context
-    init {
-        ourContext=c
-    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home__table)
 
-        val intt = intent
-        val output = intt.getStringExtra("jobtitle")
-        val dbController=DBcontrollerJob(ourContext)
-        lateinit var jobs: ArrayList<String>
-        var c:Cursor  =dbController.getAllJobs()
-        var title=c.getString(c.getColumnIndex("title"))
-        var company=c.getString(c.getColumnIndex("company"))
-        var location=c.getString(c.getColumnIndex("location"))
-        var apply=c.getString(c.getColumnIndex("apply"))
-        var status=c.getString(c.getColumnIndex("status"))
-        var view=c.getString(c.getColumnIndex("view"))
-
-        if(output==title){
-            textView1.text=title
-            textView2.text=company
-            textView3.text=location
-            textView4.text=apply
-            textView5.text=status
-            textView6.text=view
-        }
+//        val intt = intent
+//        val output = intt.getStringExtra("jobtitle")
+//        val dbController=DBcontrollerJob(ourContext)
+//        lateinit var jobs: ArrayList<String>
+//        var c:Cursor  =dbController.getAllJobs()
+//        var title=c.getString(c.getColumnIndex("title"))
+//        var company=c.getString(c.getColumnIndex("company"))
+//        var location=c.getString(c.getColumnIndex("location"))
+//        var apply=c.getString(c.getColumnIndex("apply"))
+//        var status=c.getString(c.getColumnIndex("status"))
+//        var view=c.getString(c.getColumnIndex("view"))
+//
+//        if(output==title){
+//            textView1.text=title
+//            textView2.text=company
+//            textView3.text=location
+//            textView4.text=apply
+//            textView5.text=status
+//            textView6.text=view
+//        }
 
     }
 }
